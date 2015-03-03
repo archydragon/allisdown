fd2e3aab29d0d03837fe1408f1076adf1c30bc9f
Mendor <mendor@yuuzukiyo.net>
2015-03-03 15:20:57 +0200

    fix required gems list

--- geminstall.sh
+++ geminstall.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 gem install fileutils
-gem install yaml
 gem install thin
 gem install sinatra
 gem install builder
-gem install active_support
+gem install activesupport
 
 mkdir tmp
 mkdir log
